00338b5fb5848642ed0bda598c6d19dbba5b962d
Maverick Chen <mcmaverick001@gmail.com>
2020-01-31 19:04:50 -0500

    initial game setup

---
+++ Assets/GameLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    GameLogic game;
+    List<GameObject> players;
+
+    void Awake()
+    {
+        players = new List<GameObject>();
+        if (game == null)
+        {
+            game = this;
+        }
+        else if (game != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public int RegisterPlayer(GameObject player)
+    {
+        players.Add(player);
+        return players.Count - 1;
+    }
+}
