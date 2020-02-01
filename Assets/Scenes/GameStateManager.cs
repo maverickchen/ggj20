3417617d2016ff3b69966b99931cf77539e78b3d
Maverick Chen <mcmaverick001@gmail.com>
2020-01-31 21:36:25 -0500

    make gamestatemanager available everywhere

--- Assets/Scenes/GameStateManager.cs
+++ Assets/Scenes/GameStateManager.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class GameStateManager : MonoBehaviour
 {
+    public static GameStateManager instance;
+    List<GameObject> players;
+
+    void Awake()
+    {
+        players = new List<GameObject>();
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public int RegisterPlayer(GameObject player)
+    {
+        players.Add(player);
+        return players.Count - 1;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
