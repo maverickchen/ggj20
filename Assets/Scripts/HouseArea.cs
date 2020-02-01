2bfbd07134e8de3c06d969e5c483f020c275ec58
MinSun Park <minsunp@andrew.cmu.edu>
2020-02-01 17:32:36 -0500

    dun

--- Assets/Scripts/HouseArea.cs
+++ Assets/Scripts/HouseArea.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class HouseArea : MonoBehaviour
 {
     public HouseManager houseManager;
+    private ArrayList collidingObjects = new ArrayList();
+
+    private int count = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,16 +18,42 @@ public class HouseArea : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (collidingObjects.Count > 0)
+        {
+            // keep lighting up outlines
+            count++;
+            Debug.Log(count);
+            Debug.Log("light up outlines");
+            Color color = new Color(255f, 0f, 0f, (Mathf.Sin(Time.time) + 1) / 2 * 255f);
+            GetComponent<MeshRenderer>().enabled = true;
+            GetComponent<MeshRenderer>().material.color = color;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        GameObject collidedBranch = collision.gameObject;
-        if (collidedBranch.GetComponent<Branch>())
+        GameObject collidedObject = collision.gameObject;
+        if (collidedObject.GetComponent<Branch>())
         {
-            Destroy(collidedBranch);
+            Destroy(collidedObject);
             houseManager.BranchDropped();
+        }
+        if (collidedObject.GetComponent<PlayerController>())
+        {
+            collidingObjects.Add(collidedObject);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        GameObject collidedObject = collision.gameObject;
+        if (collidedObject.GetComponent<PlayerController>())
+        {
+            collidingObjects.Remove(collidedObject);
         }
     }
 
