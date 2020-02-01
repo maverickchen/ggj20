78892094e63f4f3a16376e3824530549158f230b
Maverick Chen <mcmaverick001@gmail.com>
2020-01-31 22:24:28 -0500

    add branch pickup, setdown logic and actions

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,12 +9,16 @@ public class PlayerController : MonoBehaviour
     Vector2 movement;
     Rigidbody rb;
     public float speed = 10f;
+    public GameObject heldBranch; // the Branch GameObject being held by the player; null if player is emptyhanded
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerIndex = GameStateManager.instance.RegisterPlayer(gameObject);
+        Debug.Log("Player " + playerIndex.ToString() + " registered");
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     public void OnMove(InputValue value)
@@ -22,9 +26,34 @@ public class PlayerController : MonoBehaviour
         movement = value.Get<Vector2>();
     }
 
+    public void OnToggleBranch()
+    {
+        if (heldBranch != null)
+        {
+            anim.SetBool("HasBranch", true);
+        }
+        else
+        {
+            // get the nearest branch object 
+            GameObject nearestBranch = GetNearestBranch();
+            if (nearestBranch != null)
+            {
+                heldBranch = nearestBranch;
+                nearestBranch.SetActive(false); // make the branch invisible to players
+                anim.SetBool("HasBranch", true);
+            }
+        }
+    }
+
+    GameObject GetNearestBranch()
+    {
+        return null;
+    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Vector3 moveDir = new Vector3(movement.x, 0f, movement.y).normalized;
+        rb.MovePosition(rb.position + moveDir * speed);
     }
 }
