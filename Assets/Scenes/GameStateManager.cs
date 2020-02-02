658c481b4006b14b6d982fdebb5e03d2e5939aee
Maverick Chen <mcmaverick001@gmail.com>
2020-02-02 01:55:33 -0500

    add stun sound, add blue, red, green beavers

--- Assets/Scenes/GameStateManager.cs
+++ Assets/Scenes/GameStateManager.cs
@@ -50,7 +50,7 @@ public class GameStateManager : MonoBehaviour
         players.Add(player);
         player.transform.position = playerSpawnPositions[players.Count - 1];
         playerCards[players.Count - 1].SetActive(true);
-        if (players.Count == 3)
+        if (players.Count >= 3)
         {
             OnGameStart();
         }
