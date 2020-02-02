36f92dc65cb1e090079a4b279bb985ca7ac148c2
MinSun Park <minsunp@andrew.cmu.edu>
2020-02-01 20:33:00 -0500

    branches and tree

--- Assets/Scripts/UnderTheTree.cs
+++ Assets/Scripts/UnderTheTree.cs
@@ -6,7 +6,7 @@ public class UnderTheTree : MonoBehaviour
 {
     private float initialSpawnInterval = 1f;
     private float initialBranchesToSpawn = 5f;
-    private float spawnInterval = 5f;
+    private float spawnInterval = 4f;
     private float branchesToSpawn = 2f;
 
     private float timer;
@@ -54,10 +54,10 @@ public class UnderTheTree : MonoBehaviour
         Vector3 treeMidPosition = tree.transform.position;
         Vector3 treeSize = tree.GetComponent<Renderer>().bounds.size;
         float topOfTree = treeMidPosition.y + treeSize.y / 2;
+        float treeZ = treeMidPosition.z - treeSize.z / 2;
 
         // branch dimensions
 
-        //GameObject branch = GameObject.Find("Branch");
         Vector3 branchSize = Branch.GetComponent<Renderer>().bounds.size;
         float longestRadiusOfBranch = branchSize.x / 2;
 
@@ -65,18 +65,18 @@ public class UnderTheTree : MonoBehaviour
         Vector3 size = this.GetComponent<Renderer>().bounds.size;
         size = new Vector3(size.x - longestRadiusOfBranch, size.y - longestRadiusOfBranch, size.z - longestRadiusOfBranch);
 
-        coroutine = spawn(midPosition, size, topOfTree, numBranches);
+        coroutine = spawn(midPosition, size, topOfTree, treeZ, numBranches);
         StartCoroutine(coroutine);
     }
     
-    IEnumerator spawn(Vector3 midPosition, Vector3 size, float topOfTree, float numBranches)
+    IEnumerator spawn(Vector3 midPosition, Vector3 size, float topOfTree, float treeZ, float numBranches)
     {
         int i = 0;
         while (i < numBranches)
         {
             yield return new WaitForSeconds(initialSpawnInterval);
             float randomX = Random.Range(midPosition.x - size.x / 2, midPosition.x + size.x / 2);
-            float randomZ = Random.Range(midPosition.z - size.z / 2, midPosition.z + size.z / 2);
+            float randomZ = Random.Range(midPosition.z - size.z / 2, treeZ);
             float randomRotation = Random.Range(0, 360);
 
             Vector3 spawnPosition = new Vector3(randomX, topOfTree, randomZ);
